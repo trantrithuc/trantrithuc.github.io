066beaa228e1dabc29d21324d9d60ca6bae0d90f
tri thuc tran <tri thuc tran>
2016-03-16 20:13:27 +0700

    update

---
+++ JavaScript/Calendar/script/js.js
@@ -0,0 +1,39 @@
+﻿days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat",];
+months =["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+var mon = d.getMonth();
+function creatTable() {
+	document.write("<div id='main' style='display:none'>");
+	document.write("<TABLE BORDER ='1'>");
+	document.write("<TR>");
+	document.write("<TD><button type = 'button'>Prev</button></TD>"); 
+	document.write("<TD colspan='2'></TD>");
+	document.write("<TD colspan='3'></TD>");
+	document.write("<TD><button type = 'button'>Next</button></TD>"); 
+	document.write("</TR>");
+	filldays();
+	document.write("</TABLE>");
+	document.write("</div>");
+}
+function showTable() {
+	document.getElementById("main").style.display= "block";
+}
+function filldays() {
+	document.write("<TR>");
+	for (i=0;i<days.length;i++) {
+		document.write("<TD class='day'>");
+		document.write(days[i]);
+		document.write("</TD>");
+	}
+	document.write("</TR>");
+}
+function builMonthlist() {
+	var res="<SELECT id='ddl_month' onChange='setMonth();'>";
+	for (i = 0; i< months.length; i++) {
+		if (i == mon)
+			res+="<OPTION SELECTED= 'selected' value =" + i + ">" + months[i] +"</OPTION>";
+		else 
+			res+="<OPTION value =" + i + ">" + months[i] +"</OPTION>";
+	}
+	res+="</SELECT>";
+	return res;
+}
---
+++ JavaScript/SlideImage/script/js.js
@@ -0,0 +1,35 @@
+﻿var count = 5;
+var currentItem = 1;
+function prevAction() {
+	if (currentItem>1)
+		currentItem--;
+	else 
+		currentItem = 5; 
+	showSlide(currentItem);
+	console.log("Prev:" + currentItem)	
+}
+function nextAction() {
+	if (currentItem<count)
+		currentItem++;
+	else
+		currentItem = 1;
+	showSlide(currentItem);
+	console.log("Next:" + currentItem)
+}
+function onClickIndex(a){
+	var str = a.id;
+	currentItem = str.substr(5,1);
+	showSlide(currentItem);
+} 
+function showSlide(index) {
+	for(i = 1;i <= 5;i++) {
+		if (i == index) {
+			document.getElementById("item"+i).style.display = "block";
+			document.getElementById("index"+i).style.background = "#FFFFFF";
+		} else {
+			document.getElementById("item"+i).style.display = "none";
+			document.getElementById("index"+i).style.background = "#374C83";
+		}
+		
+	}
+}
